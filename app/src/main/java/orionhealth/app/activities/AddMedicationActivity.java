9ef50d78d7b954851f3d658bec72d65fdc10a22d
awesomeness95 <billxie27@gmail.com>
2016-04-09 20:46:48 +1000

    added add medication UI

--- app/src/main/java/orionhealth/app/activities/AddMedicationActivity.java
+++ app/src/main/java/orionhealth/app/activities/AddMedicationActivity.java
@@ -7,18 +7,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import orionhealth.app.R;
 
-public class MyMedicationActivity extends AppCompatActivity {
+public class AddMedicationActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_my_medication);
+		setContentView(R.layout.activity_add_medication);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_my_medication, menu);
+		getMenuInflater().inflate(R.menu.menu_add_medication, menu);
 		return true;
 	}
 
@@ -30,11 +30,6 @@ public class MyMedicationActivity extends AppCompatActivity {
 		int id = item.getItemId();
 
 		//noinspection SimplifiableIfStatement
-		if (id == R.id.button_add) {
-			Intent intent = new Intent(this, AddMedicationActivity.class);
-			startActivity(intent);
-			return true;
-		}
 
 		return super.onOptionsItemSelected(item);
 	}
