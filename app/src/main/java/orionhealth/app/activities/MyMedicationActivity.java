3cacc47752a1990d2fc1996f27d2e08896a12fbe
awesomeness95 <billxie27@gmail.com>
2016-04-21 09:02:56 +1000

    set up a basic tab layout

--- app/src/main/java/orionhealth/app/activities/MyMedicationActivity.java
+++ app/src/main/java/orionhealth/app/activities/MyMedicationActivity.java
@@ -6,23 +6,25 @@ package orionhealth.app.activities;
 
 import android.annotation.TargetApi;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.*;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.*;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 import orionhealth.app.R;
-import orionhealth.app.medicationDatabase.DatabaseContract.MedTableInfo;
-import orionhealth.app.medicationDatabase.MedTableOperations;
+import orionhealth.app.fragments.ListFragments.*;
+
+import java.util.Locale;
 
 public class MyMedicationActivity extends AppCompatActivity {
+
+	SectionsPagerAdapter trialPagerAdapter;
+	ViewPager mViewPager;
+	Toolbar toolbar;
+	TabLayout tabs;
 
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
@@ -30,6 +32,12 @@ public class MyMedicationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_medication);
 
+		trialPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(trialPagerAdapter);
+
+		tabs = (TabLayout) findViewById(R.id.sliding_tabs);
+		tabs.setupWithViewPager(mViewPager);
 	}
 
 	@Override
@@ -55,5 +63,76 @@ public class MyMedicationActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+	public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+		public SectionsPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			if (position == 0){
+				return new MedicationListFragment();
+			}
+			// getItem is called to instantiate the fragment for the given page.
+			// Return a PlaceholderFragment (defined as a static inner class below).
+			return PlaceholderFragment.newInstance(position + 1);
+		}
+
+		@Override
+		public int getCount() {
+			// Show 3 total pages.
+			return 5;
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			Locale l = Locale.getDefault();
+			switch (position) {
+				case 0:
+					return getString(R.string.title_section1).toUpperCase(l);
+				case 1:
+					return getString(R.string.title_section2).toUpperCase(l);
+				case 2:
+					return getString(R.string.title_section3).toUpperCase(l);
+				case 3:
+					return getString(R.string.title_section4).toUpperCase(l);
+				case 4:
+					return getString(R.string.title_section5).toUpperCase(l);
+			}
+			return null;
+		}
+	}
+
+	public static class PlaceholderFragment extends Fragment {
+		/**
+		 * The fragment argument representing the section number for this
+		 * fragment.
+		 */
+		private static final String ARG_SECTION_NUMBER = "section_number";
+
+		/**
+		 * Returns a new instance of this fragment for the given section
+		 * number.
+		 */
+		public static PlaceholderFragment newInstance(int sectionNumber) {
+			PlaceholderFragment fragment = new PlaceholderFragment();
+			Bundle args = new Bundle();
+			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+			fragment.setArguments(args);
+			return fragment;
+		}
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+								 Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_construction, container, false);
+			return rootView;
+		}
+	}
 
 }
