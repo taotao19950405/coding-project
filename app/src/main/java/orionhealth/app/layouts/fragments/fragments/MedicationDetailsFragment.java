4c573532059214d5f90e691164ca1a41ad382373
Archana Khanal <u5345703@anu.edu.au>
2016-05-23 18:43:45 +1000

    added additional fields to edit medication view, including spinner item for units

--- app/src/main/java/orionhealth/app/layouts/fragments/fragments/MedicationDetailsFragment.java
+++ app/src/main/java/orionhealth/app/layouts/fragments/fragments/MedicationDetailsFragment.java
@@ -5,7 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import ca.uhn.fhir.model.dstu2.composite.CodeableConceptDt;
 import ca.uhn.fhir.model.dstu2.resource.MedicationStatement;
@@ -16,13 +19,22 @@ import orionhealth.app.R;
  */
 public class MedicationDetailsFragment extends Fragment {
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_medication_details, container, false);
+
+        Spinner spinner = (Spinner) rootView.findViewById(R.id.unit_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.unit_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
         return rootView;
     }
+
+
+
 
     public void populateFields(MedicationStatement medicationStatement) {
         EditText nameEditTextField = (EditText) getActivity().findViewById(R.id.edit_text_name);
@@ -30,5 +42,7 @@ public class MedicationDetailsFragment extends Fragment {
         nameEditTextField.setText(codeableConcept.getText());
         EditText dosageEditTextField = (EditText) getActivity().findViewById(R.id.edit_text_dosage);
         dosageEditTextField.setText("");
+
     }
+
 }
