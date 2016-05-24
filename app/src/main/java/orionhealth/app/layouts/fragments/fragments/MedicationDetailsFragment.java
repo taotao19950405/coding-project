8b201021420ae9d8d432462191a41e1bde12551b
Archana Khanal <u5345703@anu.edu.au>
2016-05-24 21:46:55 +1000

    minor changes to layout, code for date picker temporarily commented out until resolved

--- app/src/main/java/orionhealth/app/layouts/fragments/fragments/MedicationDetailsFragment.java
+++ app/src/main/java/orionhealth/app/layouts/fragments/fragments/MedicationDetailsFragment.java
@@ -1,14 +1,21 @@
 package orionhealth.app.layouts.fragments.fragments;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import java.util.Calendar;
 
 import ca.uhn.fhir.model.dstu2.composite.CodeableConceptDt;
 import ca.uhn.fhir.model.dstu2.resource.MedicationStatement;
@@ -18,8 +25,8 @@ import orionhealth.app.R;
  * Created by bill on 25/04/16.
  */
 public class MedicationDetailsFragment extends Fragment {
-
-    @Override
+    Calendar calendar;
+        @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_medication_details, container, false);
@@ -34,8 +41,6 @@ public class MedicationDetailsFragment extends Fragment {
     }
 
 
-
-
     public void populateFields(MedicationStatement medicationStatement) {
         EditText nameEditTextField = (EditText) getActivity().findViewById(R.id.edit_text_name);
 		CodeableConceptDt codeableConcept = (CodeableConceptDt) medicationStatement.getMedication();
@@ -45,4 +50,35 @@ public class MedicationDetailsFragment extends Fragment {
 
     }
 
+//    public static class DatePickerFragment extends DialogFragment
+//            implements DatePickerDialog.OnDateSetListener {
+//
+//        @Override
+//        public Dialog onCreateDialog(Bundle savedInstanceState) {
+//            // Use the current date as the default date in the picker
+//            final Calendar c = Calendar.getInstance();
+//            int year = c.get(Calendar.YEAR);
+//            int month = c.get(Calendar.MONTH);
+//            int day = c.get(Calendar.DAY_OF_MONTH);
+//
+//            // Create a new instance of DatePickerDialog and return it
+//            return new DatePickerDialog(getActivity(), this, year, month, day);
+//        }
+//
+//        public void onDateSet(DatePicker view, int year, int month, int day) {
+//           EditText dateEditTextField = (EditText) getActivity().findViewById(R.id.edit_text_effectiveStart);
+//            dateEditTextField.setText(day + "/" + month + "/" + year);
+//        }
+//
+//
+//        public void showDatePickerDialog(View v) {
+//            DialogFragment newFragment = new DatePickerFragment();
+//            newFragment.show(getActivity().getSupportFragmentManager(), "datePicker");
+//        }
+//
+//    }
+
+
 }
+
+
